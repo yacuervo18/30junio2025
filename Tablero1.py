5d55b15b1c200d531b6dc0ae491f3e931656acee
yacuervo18 <133827557+yacuervo18@users.noreply.github.com>
2025-07-30 18:58:07 -0500

    Tablero1.py

--- Tablero1.py
+++ Tablero1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 
 
 st.set_page_config(layout='centered', page_title='Talento Teche', page_icon=':smile:')
