6954e7f222499cb123886ee3dd997df870815ceb
yacuervo18 <133827557+yacuervo18@users.noreply.github.com>
2025-07-30 19:06:23 -0500

    Tablero1.py

--- Tablero1.py
+++ Tablero1.py
@@ -36,6 +36,17 @@ with steps[0]:
     m2.metric(label= 'Metrica 2', value= np.mean(id1['Clics']),
               delta=str(np.mean(id1['Impresiones']))+'Número de rebotes', 
               delta_color='inverse')
+    
+    with steps[1]:
+    df = pd.read_csv("https://raw.githubusercontent.com/diplomado-bigdata-machinelearning-udea/Curso1/master/s03/dataVentas2009.csv")
+    df.Fecha=pd.to_datetime(df.Fecha, format='%d/%m/%Y')
+    df.set_index('Fecha', inplace=True)
+    #st.table(df)
+    varx=st.selectbox('Escoge la variable x', df.columns)
+    #vary=st.selectbox('Escoge la variable y', met_df['Clic'])
+    fig, ax = plt.subplots()
+    ax = sns.histplot(data=df, x=varx)
+    st.pyplot(fig)
 
 
 
